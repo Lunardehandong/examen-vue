d29edc27454ea320e364208f756060b41ec0a460
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2025-10-02 22:51:41 -0600

    primer avance
    creacion de proyecto y revision de las dependencias en funcionamiento

---
+++ hospitales/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="p-10">
+    <h1 class="text-4xl font-bold text-blue-500">Hola Diego 🚀</h1>
+    <button class="mt-5 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">
+      Botón Tailwind
+    </button>
+  </div>
+</template>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
